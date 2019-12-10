6d5647569eb4fce11ac7fbac95924d168748e684
Jaspreet Singh <jaspreetsinghtuli@gmail.com>
2019-12-10 22:23:25 +0530

    Implemented Driver factory

---
+++ MakeMyTripAutomation/DriverFactory/ChromeDriverManager.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium.Chrome;
+
+namespace MakeMyTripAutomation.DriverFactory
+{
+    public class ChromeDriverManager : DriverManager
+    {
+        protected override void CreateWebDriver()
+        {
+            //ChromeOptions options = new ChromeOptions();
+            this.driver = new ChromeDriver();
+        }
+    }
+}
---
+++ MakeMyTripAutomation/DriverFactory/DriverManager.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+
+namespace MakeMyTripAutomation.DriverFactory
+{
+    public abstract class DriverManager
+    {
+        protected IWebDriver driver;
+        protected abstract void CreateWebDriver();
+        public void QuitWebDriver()
+        {
+            if (null != driver)
+            {
+                driver.Quit();
+                driver = null;
+            }
+
+        }
+        public IWebDriver GetWebDriver()
+        {
+            if (driver == null)
+            {
+                CreateWebDriver();
+            }
+            return driver;
+        }
+    }
+}
