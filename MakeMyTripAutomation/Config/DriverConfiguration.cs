054871a6d46d85b9341e6227e9afa3b0f8da4fb8
Jaspreet Singh <jaspreetsinghtuli@gmail.com>
2019-12-10 22:22:39 +0530

    Added config classes

---
+++ MakeMyTripAutomation/Config/DriverConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using MakeMyTripAutomation.DriverFactory;
+using OpenQA.Selenium;
+
+namespace MakeMyTripAutomation.Config
+{
+    public class DriverConfiguration
+    {
+        private static IWebDriver driver = null;
+
+        private DriverConfiguration()
+        {
+            DriverManager driverManager = new ChromeDriverManager();
+            driver = driverManager.GetWebDriver();
+
+        }
+
+
+        public static IWebDriver GetDriver()
+        {
+            if (driver == null)
+            {
+                new DriverConfiguration();
+            }
+            driver.Manage().Window.Maximize();
+            return driver;
+        }
+
+        public static void CloseDriver()
+        {
+            driver.Close();
+            driver = null;
+        }
+
+    }
+}
